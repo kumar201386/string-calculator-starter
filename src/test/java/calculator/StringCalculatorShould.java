143e2581ca1dfdb70ec92fc7021805313e2e3689
“sonu.kushwaha” <kumar201386@yahoo.com>
2021-10-17 23:54:40 +0530

    Implementation to get number of call of Add method.

--- src/test/java/calculator/StringCalculatorShould.java
+++ src/test/java/calculator/StringCalculatorShould.java
@@ -38,4 +38,39 @@ class StringCalculatorShould {
     	StringCalculator stringCalculator = new StringCalculator();
     	assertEquals(10, stringCalculator.add("1,2;3\n\n4"));
     }
-}
+    
+    @Test
+    void HandleSumGreaderThan1000() {
+    	
+    	StringCalculator stringCalculator = new StringCalculator();
+    	assertEquals(5, stringCalculator.add("800:45,55%105"));
+    }
+    
+    @Test
+    void delimiterWithVariableLength() {
+    	
+    	StringCalculator stringCalculator = new StringCalculator();
+    	assertEquals(12, stringCalculator.add("//[***]2***5***5"));
+    	
+    }
+    
+    @Test
+    void delimiterWithMultipledelimiter() {
+    	
+    	StringCalculator stringCalculator = new StringCalculator();
+    	assertEquals(6, stringCalculator.add("//[*][%]\\n1*2%3"));
+    	
+    }
+    
+    @Test
+    void getNumberOfCallOfAddMethod() {
+    	
+    	StringCalculator stringCalculator = new StringCalculator();
+    	assertEquals(8, stringCalculator.GetCalledCount());
+    	
+    }
+    
+    
+    }
+
+
