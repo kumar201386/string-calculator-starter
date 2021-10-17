511f1fd8fde83b6ae10b486b64514492d9c19ea6
“sonu.kushwaha” <kumar201386@yahoo.com>
2021-10-17 23:33:33 +0530

    Negative number handle using runtime exception.

--- src/main/java/calculator/StringCalculator.java
+++ src/main/java/calculator/StringCalculator.java
@@ -1,42 +1,52 @@
 package calculator;
 
 class StringCalculator {
-	
-	static int count;
-	String delimiters="[,|;|\n]+";
 
-    public int add(String input)   {
-    	
-    	count++;
-    	
-    	if(input.isEmpty())
-        return 0;
-    	
-    	else {
-    			 if(input.endsWith("\n"))
-    				 throw new RuntimeException("Invalid input");
-    			 
-    			
-    		
-    		String str[] = input.split(delimiters);
-    		int sum=0;
-    		for(String s:str) {
-    			
-    			if(s!="\n")
-    			sum+=Integer.valueOf(s);
-    		
-    		}
-    		
-    		return sum;
-    		
-    		
-    		
-    	}
-    }
-    
-    public int GetCalledCount() {
-    	
-    	return count;
-    }
+	static int count;
+
+	public int add(String input) {
+
+		count++;
+
+		if (input.isEmpty())
+			return 0;
+
+		else {
+			if (input.endsWith("\n"))
+				throw new RuntimeException("Invalid input");
+
+			else {
+				int sum = 0;
+				String inputStr = input.replaceAll("[^\\d-]", " ");
+				inputStr = inputStr.trim();
+				inputStr = inputStr.replaceAll(" +", " ");
+				String arr[] = inputStr.split(" ");
+
+				for (String s : arr) {
+
+					if (checkNegative(Integer.valueOf(s))) {
+						throw new RuntimeException("Negative number !");
+					}
+
+					else {
+
+						sum += Integer.valueOf(s);
+					}
+
+				}
+
+				return sum;
+			}
+
+		}
+	}
+
+	public int GetCalledCount() {
+		return count;
+	}
+
+	public boolean checkNegative(int n) {
+		return n > 0 ? false : true;
+	}
 
 }
